9b093f6d1a86a3eeb7e12992693cf768b9657188
Ledda Bryan <louchebem06@gmail.com>
2021-06-05 03:06:40 +0200

    Update norm

--- src/client.c
+++ src/client.c
@@ -14,17 +14,17 @@
 
 static int	g_pause = 0;
 
-int	SEND_BYTE(int PID_SERVEUR, int size, char str, int us)
+int	send_byte(int pid_server, int size, char str, int us)
 {
 	while (++size < 8)
 	{
 		g_pause = 0;
 		if ((str >> size) & 1)
 		{
-			if (kill(PID_SERVEUR, SIGUSR1) == -1)
+			if (kill(pid_server, SIGUSR1) == -1)
 				return (1);
 		}
-		else if (kill(PID_SERVEUR, SIGUSR2) == -1)
+		else if (kill(pid_server, SIGUSR2) == -1)
 			return (1);
 		if (us != 1000)
 		{
@@ -38,7 +38,7 @@ int	SEND_BYTE(int PID_SERVEUR, int size, char str, int us)
 	return (0);
 }
 
-int	SEND(char *str_main, int PID_SERVEUR, int us)
+int	send(char *str_main, int pid_server, int us)
 {
 	int		i;
 	char	*str;
@@ -49,7 +49,7 @@ int	SEND(char *str_main, int PID_SERVEUR, int us)
 	str = ft_strjoin(str_main, '\n');
 	while (str[++i] != 0)
 	{
-		if (SEND_BYTE(PID_SERVEUR, size, str[i], us))
+		if (send_byte(pid_server, size, str[i], us))
 			return (1);
 	}
 	return (0);
@@ -64,14 +64,14 @@ void	handler(int seg)
 
 int	main(int ac, char **av)
 {
-	int	PID_SERVEUR;
+	int	pid_server;
 
 	signal(SIGUSR2, handler);
 	if (ac == 3)
 	{
-		PID_SERVEUR = ft_atoi(av[1]);
-		SEND(ft_itoa(getpid()), PID_SERVEUR, 1000);
-		if (!SEND(av[2], PID_SERVEUR, 0))
+		pid_server = ft_atoi(av[1]);
+		send(ft_itoa(getpid()), pid_server, 1000);
+		if (!send(av[2], pid_server, 0))
 			ft_putstr_fd("Msg is received\n", 1);
 		else
 			ft_putstr_fd("Error: server is not found\n", 1);
